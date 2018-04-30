90a0450244f59f604822c313a296248848afcd97
Tyler Gunn <tgunn@google.com>
2018-04-30 09:02:35 -0700

    Implement sendBroadcastAsUserMultiplePermissions in BridgeContext.
    sendBroadcastAsUserMultiplePermissions was added to Context.java.
    Implementing in BridgeContext.
    
    Test: Compile / build (actual usage is documented in other bugs)
    Bug: 159373687
    Change-Id: I8715b6fce7039268eb29914cb15f68ab922f4d3f
    Merged-In: I8715b6fce7039268eb29914cb15f68ab922f4d3f
    (cherry picked from commit 120908fdb15bc8c418ff5763748f41fcd2ecbaa5)

--- bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
+++ bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
@@ -1684,6 +1684,13 @@ public class BridgeContext extends Context {
     }
 
     @Override
+    public void sendBroadcastAsUserMultiplePermissions(Intent intent, UserHandle user,
+            String[] receiverPermissions) {
+        // pass
+
+    }
+
+    @Override
     public void sendBroadcast(Intent arg0, String arg1, Bundle arg2) {
         // pass
 
