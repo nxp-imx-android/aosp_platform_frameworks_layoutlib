64c116be5fc5972011fcf14bc305deaaa59d0557
Charlie Tsai <chartsai@google.com>
2018-06-20 17:26:14 +0100

    Fix android:tint attribute for Vector Drawable
    The clip bounds of given Canvas is same as the size of
    Vector Drawable, which makes the pixelize happen. Use
    original size of Canvas for temporary buffer can avoid
    this issue.
    
    Test: Added and passed the new test cases in RenderTests.
    Fixes: 63692596
    Change-Id: Id66a4dcab3e141946d0d37bdff9c6f75aff968a0

--- bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTests.java
+++ bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTests.java
@@ -19,7 +19,6 @@ package com.android.layoutlib.bridge.intensive;
 import com.android.ide.common.rendering.api.RenderSession;
 import com.android.ide.common.rendering.api.ResourceNamespace;
 import com.android.ide.common.rendering.api.ResourceReference;
-import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.rendering.api.ResourceValueImpl;
 import com.android.ide.common.rendering.api.SessionParams;
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode;
@@ -412,6 +411,61 @@ public class RenderTests extends RenderTestBase {
                 .build();
 
         renderAndVerify(params, "vector_drawable_91383.png", TimeUnit.SECONDS.toNanos(2));
+    }
+
+    /**
+     * Test a ImageView which has a vector drawable as its src and tint attribute.
+     */
+    @Test
+    public void testVectorDrawableWithTintInImageView() throws ClassNotFoundException {
+        // Create the layout pull parser.
+        LayoutPullParser parser = LayoutPullParser.createFromString(
+                "<ImageView xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                        "    android:layout_height=\"match_parent\"\n" +
+                        "    android:layout_width=\"match_parent\"\n" +
+                        "    android:src=\"@drawable/vector_drawable_without_tint\"\n" +
+                        "    android:tint=\"#FF00FF00\" />");
+        // Create LayoutLibCallback.
+        LayoutLibTestCallback layoutLibCallback =
+                new LayoutLibTestCallback(getLogger(), mDefaultClassLoader);
+        layoutLibCallback.initResources();
+
+        SessionParams params = getSessionParamsBuilder()
+                .setParser(parser)
+                .setCallback(layoutLibCallback)
+                .setTheme("Theme.Material.Light.NoActionBar.Fullscreen", false)
+                .setRenderingMode(RenderingMode.V_SCROLL)
+                .build();
+
+        renderAndVerify(params, "vector_drawable_with_tint_in_image_view.png",
+                TimeUnit.SECONDS.toNanos(2));
+    }
+
+    /**
+     * Test a vector drawable which has tint attribute.
+     */
+    @Test
+    public void testVectorDrawableWithTintInItself() throws ClassNotFoundException {
+        // Create the layout pull parser.
+        LayoutPullParser parser = LayoutPullParser.createFromString(
+                "<ImageView xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                        "    android:layout_height=\"match_parent\"\n" +
+                        "    android:layout_width=\"match_parent\"\n" +
+                        "    android:src=\"@drawable/vector_drawable_with_tint\" />");
+        // Create LayoutLibCallback.
+        LayoutLibTestCallback layoutLibCallback =
+                new LayoutLibTestCallback(getLogger(), mDefaultClassLoader);
+        layoutLibCallback.initResources();
+
+        SessionParams params = getSessionParamsBuilder()
+                .setParser(parser)
+                .setCallback(layoutLibCallback)
+                .setTheme("Theme.Material.Light.NoActionBar.Fullscreen", false)
+                .setRenderingMode(RenderingMode.V_SCROLL)
+                .build();
+
+        renderAndVerify(params, "vector_drawable_with_tint_itself.png",
+                TimeUnit.SECONDS.toNanos(2));
     }
 
     /**
