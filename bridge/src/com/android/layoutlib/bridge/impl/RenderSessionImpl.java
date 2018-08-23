edb8ffcc1003ad5600cc9ebbaec28150fa198928
Jerome Gaillard <jgaillard@google.com>
2018-08-23 13:37:13 +0100

    Use ResourceReference for passing default styles to Studio
    Bug: 74847406
    Test: to be tested on the Studio end
    Change-Id: If0c260ce1109cf10d74bce35e9a691b466ab697e
    (cherry picked from commit 462b33b94795fed2efab5671e3403c16e1dd7d5c)

--- bridge/src/com/android/layoutlib/bridge/impl/RenderSessionImpl.java
+++ bridge/src/com/android/layoutlib/bridge/impl/RenderSessionImpl.java
@@ -21,7 +21,6 @@ import com.android.ide.common.rendering.api.HardwareConfig;
 import com.android.ide.common.rendering.api.ILayoutPullParser;
 import com.android.ide.common.rendering.api.LayoutLog;
 import com.android.ide.common.rendering.api.LayoutlibCallback;
-import com.android.ide.common.rendering.api.RenderResources;
 import com.android.ide.common.rendering.api.RenderSession;
 import com.android.ide.common.rendering.api.ResourceReference;
 import com.android.ide.common.rendering.api.ResourceValue;
@@ -48,8 +47,6 @@ import com.android.layoutlib.bridge.impl.binding.FakeAdapter;
 import com.android.layoutlib.bridge.impl.binding.FakeExpandableAdapter;
 import com.android.tools.layoutlib.java.System_Delegate;
 import com.android.util.Pair;
-import com.android.util.PropertiesMap;
-import com.android.util.PropertiesMap.Property;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -1114,42 +1111,22 @@ public class RenderSessionImpl extends RenderAction<SessionParams> {
         return mSystemViewInfoList;
     }
 
-    public Map<Object, PropertiesMap> getDefaultProperties() {
-        Map<Object, PropertiesMap> defaultPropMaps = new IdentityHashMap<>();
-        RenderResources resources = getContext().getRenderResources();
-
-        Map<Object, Map<ResourceReference, ResourceValue>> namespacedPropMaps =
-                getDefaultNamespacedProperties();
-        for (Object key: namespacedPropMaps.keySet()) {
-            Map<ResourceReference, ResourceValue> namespacedPropMap = namespacedPropMaps.get(key);
-            PropertiesMap propMap = defaultPropMaps.computeIfAbsent(key,
-                    k -> new PropertiesMap(namespacedPropMap.size()));
-
-            for (ResourceReference ref: namespacedPropMap.keySet()) {
-                ResourceValue resValue = namespacedPropMap.get(ref);
-                propMap.put(ref.getResourceUrl().getQualifiedName(),
-                        new Property(resValue.getValue(),
-                                resources.resolveResValue(resValue).getValue()));
-            }
-        }
-
-        Map<Object, String> defaultStyles = getDefaultStyles();
-        for (Object key: defaultStyles.keySet()) {
-            String style = defaultStyles.get(key);
-            PropertiesMap propMap = defaultPropMaps.computeIfAbsent(key,
-                    k -> new PropertiesMap(1));
-            propMap.put("style", new Property(style, style));
-        }
-
-        return defaultPropMaps;
-    }
-
     public Map<Object, Map<ResourceReference, ResourceValue>> getDefaultNamespacedProperties() {
         return getContext().getDefaultProperties();
     }
 
     public Map<Object, String> getDefaultStyles() {
-        return getContext().getDefaultStyles();
+        Map<Object, String> defaultStyles = new IdentityHashMap<>();
+        Map<Object, ResourceReference> namespacedStyles = getDefaultNamespacedStyles();
+        for (Object key : namespacedStyles.keySet()) {
+            ResourceReference style = namespacedStyles.get(key);
+            defaultStyles.put(key, style.getQualifiedName());
+        }
+        return defaultStyles;
+    }
+
+    public Map<Object, ResourceReference> getDefaultNamespacedStyles() {
+        return getContext().getDefaultNamespacedStyles();
     }
 
     public void setScene(RenderSession session) {
