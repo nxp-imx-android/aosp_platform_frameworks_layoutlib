0b0683026afead46796201c0b0937448b0c12c73
Fedor Kudasov <kudasov@google.com>
2019-05-28 15:03:09 +0100

    Forgot to move one high quality shadow test
    Bug: N/A
    Test: N/A
    Change-Id: If09382cb0c819fa08cbf4b7aa16d3b7d8e70db9b

--- bridge/tests/src/com/android/layoutlib/bridge/intensive/HighQualityShadowsRenderTests.java
+++ bridge/tests/src/com/android/layoutlib/bridge/intensive/HighQualityShadowsRenderTests.java
@@ -31,6 +31,25 @@ public class HighQualityShadowsRenderTests extends RenderTestBase {
     }
 
     @Test
+    public void testHighQualityRectangleShadow() throws Exception {
+        LayoutPullParser parser = createParserFromPath("shadows_test.xml");
+        LayoutLibTestCallback layoutLibCallback =
+                new LayoutLibTestCallback(getLogger(), mDefaultClassLoader);
+        layoutLibCallback.initResources();
+        SessionParams params = getSessionParamsBuilder()
+                .setParser(parser)
+                .setConfigGenerator(ConfigGenerator.NEXUS_5)
+                .setCallback(layoutLibCallback)
+                .disableDecoration()
+                .build();
+
+        renderAndVerify(params, "shadows_test_high_quality.png");
+        // We expect fidelity warnings for Path.isConvex. Fail for anything else.
+        sRenderMessages.removeIf(message -> message.equals("Path.isConvex is not supported."));
+        sRenderMessages.removeIf(message -> message.equals("Font$Builder.nAddAxis is not supported."));
+    }
+
+    @Test
     public void testRoundedEdgeRectangle() throws Exception {
         LayoutPullParser parser = createParserFromPath("shadows_rounded_edge_test.xml");
         LayoutLibTestCallback layoutLibCallback =
