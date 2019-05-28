7da6cce164d8735697767285ed052ea0f8db24db
Colin Cross <ccross@android.com>
2019-05-28 14:03:41 -0700

    Convert frameworks/layoutlib to Android.bp
    See build/soong/README.md for more information.
    
    (cherry pick from I7aa398e255ef040c4a44de36b7e925673bf8bb08)
    Bug: 122333007
    Test: m sdk layoutlib layoutlib-tests libandroid_runtime
    Test: ./frameworks/layoutlib/bridge/tests/run_tests.sh
    
    Change-Id: I02f3766cd9c54bc490e651124a36b420d27d303c
    Merged-In: I7aa398e255ef040c4a44de36b7e925673bf8bb08

--- bridge/tests/run_tests.sh
+++ bridge/tests/run_tests.sh
@@ -42,7 +42,7 @@ ${STUDIO_JDK}/bin/java -ea \
     -Dtest_res.dir=${SCRIPT_DIR}/res \
     -Dtest_failure.dir=${DIST_DIR}/layoutlib_failures \
     -Djava.library.path=${NATIVE_LIBRARIES} \
-    -cp ${M2_REPO}/junit/junit/4.12/junit-4.12.jar:${M2_REPO}/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar:${MISC_COMMON}/tools-common/tools-common-prebuilt.jar:${MISC_COMMON}/sdk-common/sdk-common.jar:${MISC_COMMON}/layoutlib_api/layoutlib_api-prebuilt.jar:${MISC_COMMON}/kxml2/kxml2-2.3.0.jar:${M2_REPO}/com/google/guava/guava/22.0/guava-22.0.jar:${JAVA_LIBRARIES}/layoutlib-tests_intermediates/javalib.jar:${JAVA_LIBRARIES}/layoutlib_intermediates/javalib.jar:${JAVA_LIBRARIES}/mockito-host_intermediates/javalib.jar:${JAVA_LIBRARIES}/objenesis-host_intermediates/javalib.jar \
+    -cp ${M2_REPO}/junit/junit/4.12/junit-4.12.jar:${M2_REPO}/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar:${MISC_COMMON}/tools-common/tools-common-prebuilt.jar:${MISC_COMMON}/sdk-common/sdk-common.jar:${MISC_COMMON}/layoutlib_api/layoutlib_api-prebuilt.jar:${MISC_COMMON}/kxml2/kxml2-2.3.0.jar:${M2_REPO}/com/google/guava/guava/22.0/guava-22.0.jar:${JAVA_LIBRARIES}/layoutlib-tests_intermediates/javalib.jar:${JAVA_LIBRARIES}/layoutlib_intermediates/javalib.jar \
     org.junit.runner.JUnitCore \
     com.android.layoutlib.bridge.intensive.Main
 
