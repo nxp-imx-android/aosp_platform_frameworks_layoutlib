836aae6d32359075f6871a687a6a552f8492c293
Jerome Gaillard <jgaillard@google.com>
2019-10-10 12:08:01 +0100

    Move comment that was left behind
    The comment should have been moved with the code it is related to.
    
    Bug: N/A
    Test: N/A
    Change-Id: I646355eb78752ff80ee87d756199e5f62c53c85d

--- bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTestBase.java
+++ bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTestBase.java
@@ -165,6 +165,9 @@ public class RenderTestBase {
     private static String getFontDir() {
         String fontDir = System.getProperty(FONT_DIR_PROPERTY);
         if (fontDir == null) {
+            // The fonts are built into out/host/common/obj/PACKAGING/fonts_intermediates
+            // as specified in build/make/core/layoutlib_fontst.mk, and PLATFORM_DIR is
+            // out/host/[arch]/sdk/sdk*/android-sdk*/platforms/android*
             fontDir = PLATFORM_DIR +
                     "/../../../../../../common/obj/PACKAGING/fonts_intermediates";
         }
@@ -348,8 +351,6 @@ public class RenderTestBase {
                 };
         sProjectResources.loadResources();
 
-        // The fonts are built into out/host/common/obj/PACKAGING/fonts_intermediates as specified in
-        // build/make/core/layoutlib_fonts.mk, and PLATFORM_DIR is out/host/[arch]/sdk/sdk*/android-sdk*/platforms/android*
         File fontLocation = new File(FONT_DIR);
         File buildProp = new File(PLATFORM_DIR, "build.prop");
         File attrs = new File(res, "values" + File.separator + "attrs.xml");
