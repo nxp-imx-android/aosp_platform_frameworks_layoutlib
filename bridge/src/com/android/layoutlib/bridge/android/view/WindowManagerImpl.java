79cf8a03a29e93bef8c8d1f627dda739771dec79
Jerome Gaillard <jgaillard@google.com>
2020-01-31 14:26:17 +0000

    Update layoutlib WindowManagerImpl following commit 10117238
    Change I328b1b5a added new methods in WindowManager. This updates the
    layoutlib version WindowManagerImpl by copying the relevant code from
    the Android version.
    
    Bug: 74062470
    Test: layoutlib tests
    Change-Id: I43469cd71f294d57b9d76169f7ac694c44fc1298

--- bridge/src/com/android/layoutlib/bridge/android/view/WindowManagerImpl.java
+++ bridge/src/com/android/layoutlib/bridge/android/view/WindowManagerImpl.java
@@ -15,21 +15,34 @@
  */
 package com.android.layoutlib.bridge.android.view;
 
+import android.app.ResourcesManager;
+import android.content.Context;
+import android.graphics.Insets;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.Region;
+import android.os.RemoteException;
 import android.util.DisplayMetrics;
+import android.util.Size;
 import android.view.Display;
 import android.view.Display.Mode;
 import android.view.DisplayAdjustments;
+import android.view.DisplayCutout;
 import android.view.DisplayInfo;
 import android.view.View;
+import android.view.WindowInsets;
 import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
+import android.view.WindowMetrics;
 
 public class WindowManagerImpl implements WindowManager {
 
+    private final Context mContext;
     private final DisplayMetrics mMetrics;
     private final Display mDisplay;
 
-    public WindowManagerImpl(DisplayMetrics metrics) {
+    public WindowManagerImpl(Context context, DisplayMetrics metrics) {
+        mContext = context;
         mMetrics = metrics;
 
         DisplayInfo info = new DisplayInfo();
@@ -92,5 +105,51 @@ public class WindowManagerImpl implements WindowManager {
     @Override
     public void setShouldShowIme(int displayId, boolean shouldShow) {
         // pass
+    }
+
+    @Override
+    public WindowMetrics getCurrentWindowMetrics() {
+        final Rect bound = getCurrentBounds(mContext);
+
+        return new WindowMetrics(toSize(bound), computeWindowInsets());
+    }
+
+    private static Rect getCurrentBounds(Context context) {
+        synchronized (ResourcesManager.getInstance()) {
+            return context.getResources().getConfiguration().windowConfiguration.getBounds();
+        }
+    }
+
+    @Override
+    public WindowMetrics getMaximumWindowMetrics() {
+        return new WindowMetrics(toSize(getMaximumBounds()), computeWindowInsets());
+    }
+
+    private Size toSize(Rect frame) {
+        return new Size(frame.width(), frame.height());
+    }
+
+    private Rect getMaximumBounds() {
+        final Point displaySize = new Point();
+        mDisplay.getRealSize(displaySize);
+        return new Rect(0, 0, displaySize.x, displaySize.y);
+    }
+
+    private WindowInsets computeWindowInsets() {
+        try {
+            final Rect systemWindowInsets = new Rect();
+            final Rect stableInsets = new Rect();
+            final DisplayCutout.ParcelableWrapper displayCutout =
+                    new DisplayCutout.ParcelableWrapper();
+            WindowManagerGlobal.getWindowManagerService().getWindowInsets(
+                    new WindowManager.LayoutParams(), mContext.getDisplayId(), systemWindowInsets,
+                    stableInsets, displayCutout);
+            return new WindowInsets.Builder()
+                    .setSystemWindowInsets(Insets.of(systemWindowInsets))
+                    .setStableInsets(Insets.of(stableInsets))
+                    .setDisplayCutout(displayCutout.get()).build();
+        } catch (RemoteException ignore) {
+        }
+        return null;
     }
 }
