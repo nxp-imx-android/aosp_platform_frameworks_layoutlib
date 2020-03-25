62053d01ac439c4c97a6cd6533e456ece645876a
Fedor Kudasov <kudasov@google.com>
2020-03-25 11:20:01 +0000

    Improve handler callbacks disposal
    - Wrap each callback execution into a safe call
    - Remove all callbacks for the current BridgeContext
    even if new are added
    
    Bug: 146552571
    Test: all tests should pass
    Change-Id: Ic6d42d99bf9fea74e326164a8397b4251bcdef58

--- bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTests.java
+++ bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTests.java
@@ -176,6 +176,10 @@ public class RenderTests extends RenderTestBase {
                 .build();
 
         renderAndVerify(params, "allwidgets.png");
+
+        // Caused by Editor$Blink
+        sRenderMessages.removeIf(message -> message.equals(
+                "Infinite loop detected while executing Handler callbacks"));
     }
 
     @Test
@@ -196,6 +200,10 @@ public class RenderTests extends RenderTestBase {
                 .disableShadows()
                 .build();
         renderAndVerify(params, "allwidgets_tab.png");
+
+        // Caused by Editor$Blink
+        sRenderMessages.removeIf(message -> message.equals(
+                "Infinite loop detected while executing Handler callbacks"));
     }
 
     @Test
