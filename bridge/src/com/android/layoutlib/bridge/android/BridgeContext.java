3198d54a717fc4a5fb67580e8255d84097c9c4e8
Jerome Gaillard <jgaillard@google.com>
2020-08-26 15:48:24 +0100

    Add activity service support
    Creates an ActivityManager inside BridgeContext to provide an activity
    service.
    
    Bug: 164003227
    Test: N/A
    Change-Id: Ic650d4cd3163878a1361c44a99a4a25342d4f1ca
    (cherry picked from commit e9c8837d18ede98875da22f4a8f79cf6cd95fe4d)

--- bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
+++ bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
@@ -41,6 +41,8 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.app.ActivityManager;
+import android.app.ActivityManager_Accessor;
 import android.app.SystemServiceRegistry;
 import android.content.BroadcastReceiver;
 import android.content.ClipboardManager;
@@ -166,6 +168,7 @@ public class BridgeContext extends Context {
     private final DisplayManager mDisplayManager;
     private final AutofillManager mAutofillManager;
     private final ClipboardManager mClipboardManager;
+    private final ActivityManager mActivityManager;
     private final HashMap<View, Integer> mScrollYPos = new HashMap<>();
     private final HashMap<View, Integer> mScrollXPos = new HashMap<>();
 
@@ -253,6 +256,7 @@ public class BridgeContext extends Context {
         mDisplayManager = new DisplayManager(this);
         mAutofillManager = new AutofillManager(this, new Default());
         mClipboardManager = new ClipboardManager(this, null);
+        mActivityManager = ActivityManager_Accessor.getActivityManagerInstance(this);
 
         if (mLayoutlibCallback.isResourceNamespacingRequired()) {
             if (mLayoutlibCallback.hasAndroidXAppCompat()) {
@@ -647,6 +651,9 @@ public class BridgeContext extends Context {
 
             case CLIPBOARD_SERVICE:
                 return mClipboardManager;
+
+            case ACTIVITY_SERVICE:
+                return mActivityManager;
 
             case AUDIO_SERVICE:
             case TEXT_CLASSIFICATION_SERVICE:
